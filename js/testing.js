21db9c812e300e8fdf684715d4a2747de8e7de63
Quasark <q@quasark.xyz>
2023-08-01 15:22:19 -0700

    API question implementation

--- js/testing.js
+++ js/testing.js
@@ -3,8 +3,7 @@
 function fetchQuizData(url) {
   // GPT helping out with the API pull request
 
-  // fetch('https://opentdb.com/api.php?amount=10&category=20&difficulty=easy&type=multiple')
-  fetch(url)
+  return fetch(url)
     .then(response => {
       if (!response.ok) {
         throw new Error('Network response was not ok');
@@ -13,71 +12,37 @@ function fetchQuizData(url) {
     })
     .then(data => {
       // Use the data here
-      console.log(data);
+      // console.log(data);
+      return(data);
     })
     .catch(error => {
       console.error('Error fetching quiz data:', error);
     });
 }
 
-let urlToGet = 'https://opentdb.com/api_category.php';
-
-fetchQuizData(urlToGet);
-
-
-// // Function to fetch the JSON data from the file
-// async function fetchQuizData() {
-//   try {
-//     const response = await fetch('./quizData.json');
-//     const data = await response.json();
-//     // Once the data is fetched, you can use it in your app
-//     console.log(data); // Do something with the data
-//   } catch (error) {
-//     console.error('Error fetching quiz data:', error);
-//   }
-// }
-
-// Call the function to fetch the data
-// fetchQuizData();
+// let urlToGet = 'https://opentdb.com/api_category.php';
+let urlToGet = 'https://opentdb.com/api.php?amount=10&category=20&difficulty=easy&type=multiple';
 
 
-// function Question(question,correctAnswer, difficultyLevel) {
-//   player.questions.push({
-//     question: question,
-//     choices: choices,
-//     correctAnswer: correctAnswer,
-//     difficultyLevel: difficultyLevel, // Added difficulty level to the question
-//   });
-// }  
+function fetchAndReturnQuestionData() {
+  return fetchQuizData(urlToGet)
+    .then(quizData => {
+      if (quizData.response_code === 0) {
+        return quizData.results;
+      } else {
+        console.log('There was an error collecting questions: ' + quizData.response_code);
+      }
+    });
+}
+
+function askQuestions(questionPool) {
+  console.log('This is where your question function can go. All questions are in questionPool');
+  console.log(questionPool);
+}
 
 
-// JSON {
-//   difficultyLevel: 
-//     level: 1
-//     questions:  {
-//       category: {
-//         movies: {
-//           question: "Question goes here",
-//           correctAnswer: "2001",
-//           incorrectAnswer: ["1999", "1888"],
-//         },
-//         anime: {
-    
-//         },
-//     }
-//   }
-// }
 
+let questionPool = fetchAndReturnQuestionData();
+questionPool.then(askQuestions);
 
-// let questionPool = {
-//   questionText: "A slug's blood is green.", 
-//   questionNumber: 1,
-//   correctAnswer: "t",
-// };
-
-// let questionPool = {
-//   questionText: "What color is a slug's", 
-//   questionNumber: 1,
-//   correctAnswer: "Green",
-//   incorrectAnswers: ["Blue", "yellow", "pink"],
-// };
+// fetchAndReturnQuestionData.then(askQuestions);
