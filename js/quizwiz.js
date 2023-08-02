6570ee8ac808792ba3f6b1c38cc2748379ebac7f
Chester Lee Coloma <cleecoloma@gmail.com>
2023-08-01 19:08:58 -0700

    updated quiz js with data object similar to end goal

--- js/quizwiz.js
+++ js/quizwiz.js
@@ -2,46 +2,37 @@
 
 console.log('quizwiz.js loaded');
 
-function playQuizWiz (activePlayer, quizData) {
-  console.log('We\'re ready to play!');
-  console.log(activePlayer);
-  console.log(quizData); 
-}
-
-// name: string
-// Category: Drop down list
-// # Questions they want to be asked
-// difficulty (E/M/H)
-
-const questionData = [
-  {
-    category: 'Mythology',
-    type: 'multiple',
-    difficulty: 'easy',
-    question:
-      'Who was the only god from Greece who did not get a name change in Rome?',
-    correct_answer: 'Apollo',
-    incorrect_answers: ['Zeus', 'Hermes', 'Poseidon'],
-  },
-  {
-    category: 'Mythology',
-    type: 'multiple',
-    difficulty: 'easy',
-    question:
-      'The ancient Roman god of war was commonly known as which of the following?',
-    correct_answer: 'Mars',
-    incorrect_answers: ['Jupiter', 'Mercury', 'Venus'],
-  },
-  {
-    category: 'Mythology',
-    type: 'multiple',
-    difficulty: 'easy',
-    question: 'Who was the King of Gods in Ancient Greek mythology?',
-    correct_answer: 'Zeus',
-    incorrect_answers: ['Hera', 'Poseidon', 'Ares'],
-  },
-  // Add more questions as needed
-];
+const questionData = {
+  response_code: 0,
+  results: [
+    {
+      category: 'Mythology',
+      type: 'multiple',
+      difficulty: 'easy',
+      question:
+        'Who was the only god from Greece who did not get a name change in Rome?',
+      correct_answer: 'Apollo',
+      incorrect_answers: ['Zeus', 'Hermes', 'Poseidon'],
+    },
+    {
+      category: 'Mythology',
+      type: 'multiple',
+      difficulty: 'easy',
+      question:
+        'The ancient Roman god of war was commonly known as which of the following?',
+      correct_answer: 'Mars',
+      incorrect_answers: ['Jupiter', 'Mercury', 'Venus'],
+    },
+    {
+      category: 'Mythology',
+      type: 'multiple',
+      difficulty: 'easy',
+      question: 'Who was the King of Gods in Ancient Greek mythology?',
+      correct_answer: 'Zeus',
+      incorrect_answers: ['Hera', 'Poseidon', 'Ares'],
+    },
+  ],
+};
 
 function Player(playerName) {
   this.name = playerName;
@@ -58,20 +49,24 @@ function Player(playerName) {
 
 let player = new Player('Chester');
 
+
 const NUMBER_ANSWER_OPTION = 4;
 
-function randomAnswerArray(questionData) {
+function randomAnswerArray(answers) {
   const answerArray = [];
   let randomIndex = Math.floor(Math.random() * NUMBER_ANSWER_OPTION);
-  let incorrectAnswer = questionData.incorrect_answers;
+  let incorrectAnswer = answers.incorrect_answers;
+  console.log(incorrectAnswer);
 
   for (let i = 0; i < NUMBER_ANSWER_OPTION; i++) {
+    console.log(randomIndex);
     if (i === randomIndex) {
-      answerArray.push(questionData.correct_answer);
+      answerArray.push(answers.correct_answer);
     } else {
       answerArray.push(incorrectAnswer.pop());
     }
   }
+  console.log(answerArray);
   return answerArray;
 }
 
@@ -79,10 +74,10 @@ let currentQuestion = 0;
 const questionTextElement = document.getElementById('question-text');
 const optionsElement = document.getElementById('options');
 const nextBtn = document.getElementById('nextBtn');
-const selectedAnswers = new Array(questionData.length).fill(null);
+const selectedAnswers = new Array(questionData.results.length).fill(null);
 
 function loadQuestion() {
-  const currentQuestionData = questionData[currentQuestion];
+  const currentQuestionData = questionData.results[currentQuestion];
   questionTextElement.textContent = currentQuestionData.question;
   optionsElement.innerHTML = '';
   const options = randomAnswerArray(currentQuestionData);
@@ -112,28 +107,25 @@ function loadQuestion() {
     optionsElement.appendChild(li);
   }
   nextBtn.textContent =
-    currentQuestion === questionData.length - 1 ? 'Submit' : 'Next';
+    currentQuestion === questionData.results.length - 1 ? 'Submit' : 'Next';
 }
 
 function evaluateAnswer(userAnswer, actualAnswer) {
   if (userAnswer === actualAnswer) {
     player.currentCorrectAnswers++;
     player.currentNumberAskedQuestions++;
-    console.log('Hihi');
   } else {
     player.currentNumberAskedQuestions++;
   }
 }
 
 function goToNextQuestion() {
-  if (currentQuestion < questionData.length - 1) {
+  if (currentQuestion < questionData.results.length - 1) {
     currentQuestion++;
     evaluateAnswer(
       selectedAnswers[currentQuestion - 1],
-      questionData[currentQuestion - 1].correct_answer
+      questionData.results[currentQuestion - 1].correct_answer
     );
-    // console.log(selectedAnswers[currentQuestion-1]);
-    // console.log(questionData[currentQuestion-1].correct_answer);
     console.log(player);
     loadQuestion();
   } else {
@@ -145,9 +137,9 @@ function goToNextQuestion() {
 function showResults() {
   // Display the user's selected answers (you can customize the output as per your requirements)
   let resultsHTML = '<h2>Results</h2>';
-  for (let i = 0; i < questionData.length; i++) {
+  for (let i = 0; i < questionData.results.length; i++) {
     const userAnswer = selectedAnswers[i];
-    const correctAnswer = questionData[i].correct_answer;
+    const correctAnswer = questionData.results[i].correct_answer;
     const isCorrect = userAnswer === correctAnswer;
     resultsHTML += `<p>Question ${
       i + 1
@@ -158,7 +150,7 @@ function showResults() {
   document.body.innerHTML = resultsHTML;
 }
 
-// nextBtn.addEventListener('click', goToNextQuestion);
+nextBtn.addEventListener('click', goToNextQuestion);
 
 // Load the first question
 loadQuestion();
